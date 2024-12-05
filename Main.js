473509dad93c819b6a6d801de3406486b2ac48f6
YerkoBizerko <145455618+YerkoBizerko@users.noreply.github.com>
2024-12-06 09:00:10 +1300

    Feature:
    1. Added dynamic cart counter to Main.JS
    2. Add to cart buttons in Shop.HTML now funtional
    3. Added Cart.JS to file explorer

--- Main.js
+++ Main.js
@@ -58,12 +58,14 @@ let ListProducts = [{
 }
 ];
 
+let basket = [];
+
 let AddDataWebsite = () => {
     return (ListProduct.innerHTML = ListProducts
         .map((x) => {
             let {id, name, price, image, desc} = x;
             return `
-        <div id=product-id-${id} class="col-xl-3 col-lg-3 col-md-4 col-sm-6 item">
+        <div id=product-id-${id} class="col-xl-3 col-lg-3 col-md-4 col-sm-6">
           <div class="card h-100">
             <img src=${image} class="card-img-top" alt="Product Image">
             <div class="card-body">
@@ -71,7 +73,7 @@ let AddDataWebsite = () => {
               <p class="card-text fs-5">$${price}</p>
               <p class="card-text fw-light fs-5">${desc}</p>
               <div class="d-flex justify-content-center mt-4">
-                <button type="button" class="btn btn-danger my-2 AddToCart">Add to cart</button>
+                <button type="button" class="btn btn-danger my-2" onclick="AddCart(${id})">Add to cart</button>
               </div>
             </div>
           </div>
@@ -80,3 +82,23 @@ let AddDataWebsite = () => {
         }).join(""));
 };
 AddDataWebsite();
+
+let AddCart = (id) => {
+  let search = basket.find((product) => product.id === id)
+
+  if (search === undefined){
+    basket.push({
+      id: id,
+      item: 1,
+    });
+  }
+  else {
+    search.item +=1
+  }
+  CartCalculation();
+};
+
+let CartCalculation = () => {
+  let CartCounter = document.getElementById("CartAmount");
+  CartCounter.innerHTML = basket.map((x) => x.item).reduce((x, y) => x + y, 0)
+}
