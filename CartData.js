61dd69673b865b79f299e9c3a119c86d7ebe68ad
Sione Yerkovich <sioneyerkovich01@gmai.com>
2024-12-10 19:22:42 +1300

    Feature/Format:
    Feature:
    1. Added increment/decrement function to items in cart. Minimum value of 1 applied to function
    
    Format:
    1. Moved product data to CartData.JS for less clutter
    2. Commented out all HTML from Cart.html and migrated to Cart.JS for future function development

---
+++ CartData.js
@@ -0,0 +1,57 @@
+let ListProducts = [{
+    id: 1,
+    name: "Organic Dog Food",
+    price: 23.99,
+    image: "../Images/dogfood.jpg",
+    desc: "Natural food for your furry friend."
+},
+{
+    id: 2,
+    name: "Cat Scratching Post",
+    price: 44.99,
+    image: "../Images/cat_scratcher.jpg",
+    desc:"Durable playground for your cat."
+},
+{
+    id: 3,
+    name: "Bird Feeder",
+    price: 99.99,
+    image: "../Images/bird_feeder.jpg",
+    desc: "Spacious and stylish for your bird."
+},
+{
+  id: 4,
+  name: "Cat Scratching Post",
+  price: 49.99,
+  image: "../Images/cat_scratcher.jpg",
+  desc:"Durable playground for your cat."
+},
+{
+  id: 5,
+  name: "Bird Feeder",
+  price: 99.99,
+  image: "../Images/bird_feeder.jpg",
+  desc: "Spacious and stylish for your bird."
+},
+{
+  id: 6,
+  name: "Organic Dog Food",
+  price: 23.99,
+  image: "../Images/dogfood.jpg",
+  desc: "Natural food for your furry friend."
+},
+{
+  id: 7,
+  name: "Cat Scratching Post",
+  price: 49.99,
+  image: "../Images/cat_scratcher.jpg",
+  desc:"Durable playground for your cat."
+},
+{
+  id: 8,
+  name: "Organic Dog Food",
+  price: 23.99,
+  image: "../Images/dogfood.jpg",
+  desc: "Natural food for your furry friend."
+}
+];
--- Main.js
+++ Main.js
@@ -1,63 +1,5 @@
 let ListProduct = document.querySelector('.ListProduct');
 
-let ListProducts = [{
-    id: 1,
-    name: "Organic Dog Food",
-    price: 23.99,
-    image: "../Images/dogfood.jpg",
-    desc: "Natural food for your furry friend."
-},
-{
-    id: 2,
-    name: "Cat Scratching Post",
-    price: 44.99,
-    image: "../Images/cat_scratcher.jpg",
-    desc:"Durable playground for your cat."
-},
-{
-    id: 3,
-    name: "Bird Feeder",
-    price: 99.99,
-    image: "../Images/bird_feeder.jpg",
-    desc: "Spacious and stylish for your bird."
-},
-{
-  id: 4,
-  name: "Cat Scratching Post",
-  price: 49.99,
-  image: "../Images/cat_scratcher.jpg",
-  desc:"Durable playground for your cat."
-},
-{
-  id: 5,
-  name: "Bird Feeder",
-  price: 99.99,
-  image: "../Images/bird_feeder.jpg",
-  desc: "Spacious and stylish for your bird."
-},
-{
-  id: 6,
-  name: "Organic Dog Food",
-  price: 23.99,
-  image: "../Images/dogfood.jpg",
-  desc: "Natural food for your furry friend."
-},
-{
-  id: 7,
-  name: "Cat Scratching Post",
-  price: 49.99,
-  image: "../Images/cat_scratcher.jpg",
-  desc:"Durable playground for your cat."
-},
-{
-  id: 8,
-  name: "Organic Dog Food",
-  price: 23.99,
-  image: "../Images/dogfood.jpg",
-  desc: "Natural food for your furry friend."
-}
-];
-
 let basket = JSON.parse(localStorage.getItem("SavedCart")) || [];
 
 let AddDataWebsite = () => {
@@ -102,6 +44,6 @@ let AddCart = (id) => {
 let CartCalculation = () => {
   let CartCounter = document.getElementById("CartAmount");
   CartCounter.innerHTML = basket.map((x) => x.item).reduce((x, y) => x + y, 0)
-}
+};
 
 CartCalculation();
