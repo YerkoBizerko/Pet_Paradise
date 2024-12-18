9cefe5400e0293a4e7e6508044133974a9d7c4fe
Sione Yerkovich <sioneyerkovich01@gmai.com>
2024-12-18 20:06:27 +1300

    Format:
    1. Changed image src for all images on all pages to troubleshoot with github pages

--- CartData.js
+++ CartData.js
@@ -2,56 +2,56 @@ let ListProducts = [{
     id: 1,
     name: "Organic Dog Food",
     price: 23.99,
-    image: "../Images/dogfood.jpg",
+    image: "./images/dogfood.jpg",
     desc: "Natural food for your furry friend."
 },
 {
     id: 2,
     name: "Cat Scratching Post",
     price: 44.99,
-    image: "../Images/cat_scratcher.jpg",
+    image: "./images/cat_scratcher.jpg",
     desc:"Durable playground for your cat."
 },
 {
     id: 3,
     name: "Bird Feeder",
     price: 99.99,
-    image: "../Images/bird_feeder.jpg",
+    image: "./images/bird_feeder.jpg",
     desc: "Spacious and stylish for your bird."
 },
 {
   id: 4,
   name: "Raw Dog Food",
   price: 9.99,
-  image: "../Images/RawDogFood.jpg",
+  image: "./images/RawDogFood.jpg",
   desc:"Quick and easy solution for bestie."
 },
 {
   id: 5,
   name: "Birdseed Bag",
   price: 14.45,
-  image: "../Images/BirdSeed.jpg",
+  image: "./images/BirdSeed.jpg",
   desc: "Keep you birds well fed and nourished."
 },
 {
   id: 6,
   name: "Tennis Ball",
   price: 1.99,
-  image: "../Images/TennisBall.jpg",
+  image: "./images/TennisBall.jpg",
   desc: "Hours of fun for various pet types."
 },
 {
   id: 7,
   name: "Dog Pool",
   price: 169.99,
-  image: "../Images/DogPool.jpg",
+  image: "./images/DogPool.jpg",
   desc:"Bring the beach home with this pool."
 },
 {
   id: 8,
   name: "Cat Streamer",
   price: 6.39,
-  image: "../Images/CatStreamer.jpg",
+  image: "./images/CatStreamer.jpg",
   desc: "Get them moving with this fun toy."
 }
 ];
